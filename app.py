ae5710ab41e369a2304fa681f93a11e1b204c20b
SANEKNAYMCHIK <sashanaumov053@yandex.ru>
2025-11-17 18:22:41 +0300

    Added uploading files and func to call launch algorithms

--- app.py
+++ app.py
@@ -37,12 +37,39 @@ class SimpleApp:
         
         self.label_file2 = ttk.Label(btn_frame, text="Файл 2 не выбран")
         self.label_file2.pack(side=tk.RIGHT, padx=5)
+        
+        self.submit_btn = ttk.Button(
+            self.root,
+            text="Выполнить",
+            command=self.run_methods
+        )
+        self.submit_btn.pack(pady=10)
     
+
     def load_file1(self):
-        return
+        file_path = filedialog.askopenfilename(title="Выберите первый файл")
+        if file_path:
+            self.file1_path = file_path
+            self.label_file1.config(text=f"Файл 1: {file_path.split('/')[-1]}")
     
+
     def load_file2(self):
-        return
+        file_path = filedialog.askopenfilename(title="Выберите второй файл")
+        if file_path:
+            self.file2_path = file_path
+            self.label_file2.config(text=f"Файл 2: {file_path.split('/')[-1]}")
+    
+    def run_methods(self):
+        data = {
+            "file1": self.file1_path,
+            "file2": self.file2_path,
+        }
+        # parser(data['file1'])
+        # parser(data['file2'])
+        # method_1(file1, file2)
+        # method_2(file1, file2)
+        # method_3(file1, file2)
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
